0a1d5c1091f68645275bb78be3d33c4209f8f263
kalundij-ai <kalundij@gmail.com>
2026-04-20 01:25:50 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+export const members = [
+  {
+    id: "1",
+    name: "Kassim Juma Kalundi",
+    clan: "Kalundi",
+    gender: "Male",
+    parentId: "",
+    spouseId: "2",
+    birthDate: "",
+    photo: ""
+  },
+  {
+    id: "2",
+    name: "Hajjat Leila Shomari Sewando",
+    clan: "Sewando",
+    gender: "Female",
+    parentId: "",
+    spouseId: "1",
+    birthDate: "",
+    photo: ""
+  },
+  {
+    id: "3",
+    name: "Haidari",
+    clan: "Kalundi",
+    gender: "Male",
+    parentId: "1",
+    spouseId: "",
+    birthDate: "",
+    photo: ""
+  },
+  {
+    id: "4",
+    name: "Jamila",
+    clan: "Kalundi",
+    gender: "Female",
+    parentId: "1",
+    spouseId: "",
+    birthDate: "",
+    photo: ""
+  }
+];
